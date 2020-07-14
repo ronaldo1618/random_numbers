da6aef3c6e91291e22d45675afcc1c2e6843ad35
Ronald Lankford <rolankford@gmail.com>
2020-07-14 10:01:06 -0500

    init

---
+++ random_numbers.py
@@ -0,0 +1,31 @@
+import random
+"""
+Print a message to the console indicating whether each value of
+`number` is in the `my_randoms` list.
+"""
+
+my_randoms = list()
+for i in range(10):
+    my_randoms.append(random.randrange(1, 6))
+
+# Generate a list of numbers 1..10
+numbers_1_to_10 = range(1, 11)
+
+# O(n^2)
+for number in numbers_1_to_10:
+    the_numbers_match = False
+    for num in my_randoms:
+        if num == number:
+            the_numbers_match = True
+    if the_numbers_match:
+        print(f'{number} is in the random list')
+    else:
+        print(f'{number} is NOT in the random list')
+print(my_randoms)
+
+# O(n)
+my_randoms = set(my_randoms)
+numbers_1_to_10 = set(numbers_1_to_10)
+exist = my_randoms.intersection(numbers_1_to_10)
+dont_exist = (exist^numbers_1_to_10)&numbers_1_to_10
+print(f'These numbers exist in the my_randoms list: {list(exist)} and these numbers do not: {list(dont_exist)}')
